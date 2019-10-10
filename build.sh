fccdf5a21c9e9f5cbebb7751304014dd02cf7605
chebyrash <pd.stroganov@gmail.com>
2019-10-10 20:28:58 +0100

    Removed scripts

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-docker rmi chebyrash/channy-one
-docker build -t chebyrash/channy-one .
--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-docker kill $(docker ps -aq)
-docker rm $(docker ps -aq)
-docker rmi $(docker images -aq)
-
-docker run -d -p 80:80 --restart=unless-stopped --log-opt max-size=10m --log-opt mode=non-blocking chebyrash/channy-one
